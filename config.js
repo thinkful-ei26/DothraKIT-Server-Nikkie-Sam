805b9be3a7ce70129242d35ad9a6ca114e520077
Sam Smiley <samtsmiley@gmail.com>
2019-01-28 10:49:04 -0800

    added db conneciton

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   JWT_EXPIRY: process.env.JWT_EXPIRY || '1d',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/dothrakit',
+        process.env.MONGODB_URI || 'mongodb://localhost/dothrakit',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test'
