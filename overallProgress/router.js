c2051c6a49826812987d0279bfef6fc1f37cc617
Nikkie Mashian <nikkiemashian@gmail.com>
2019-02-01 07:34:17 -0800

    changed overall progress logic - if they havent made any guesses yet, should still be 0

--- overallProgress/router.js
+++ overallProgress/router.js
@@ -15,7 +15,8 @@ router.get('/:userId', (req, res) => {
       let totalGuesses = user.words.map(words => words.totalTries).reduce((prev, next) => prev + next);
       let totalCorrect = user.words.map(words => words.totalCorrect).reduce((prev, next) => prev + next);
       console.log('THE OVERALLPROGRESS SENT BACK IN FETCH IS', Math.floor(100 *(totalCorrect/totalGuesses)));
-      res.status(200).json(Math.floor(100 *(totalCorrect/totalGuesses)));
+      let progress = totalGuesses!==0 ? Math.floor(100 *(totalCorrect/totalGuesses)) : 0;
+      res.status(200).json(progress);
     });
 });
 
